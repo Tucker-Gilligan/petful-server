7b2e9ca5056b58775bd5afa5cfd700d8293939e6
Tucker <gillitux@gmail.com>
2021-03-04 13:58:18 -0500

    fixing CORS issues still

--- modules/config.js
+++ modules/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   CLIENT_ORIGIN:
-    'https://tucker-petful-client.vercel.app/' || 'http://localhost:3000',
+    'https://tucker-petful-client.vercel.app' || 'http://localhost:3000',
   PORT: process.env.PORT || 8080,
   NODE_ENV: process.env.NODE_ENV || 'development',
 };
